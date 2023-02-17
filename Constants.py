33f1cfcdcb212bb98290f33ec6d4fd80a290b860
Joel <1105039@est.intec.edu.do>
2023-02-16 22:22:04 -0400

    Cambios ligeros

--- Constants.py
+++ Constants.py
@@ -1,2 +1,4 @@
+# variables constantes para trabajar todo por medio de parametrización
+
 WALLPAPER_PATH = 'C:\\Users\\mauri\\Desktop\\Wallpapers\\km7qt7z3veg51.jpg'
 WALLPAPER_PATH2 = 'C:\\Users\\mauri\\Desktop\\Wallpapers\\w5.png'
